9d4edbdc52d206cb8c00b0ad6931c1407e4c510d
alimertcetin <mrtmert54@gmail.com>
2025-10-21 19:52:32 +0300

    feat: XIVEcsUnityIntegration

---
+++ OpachaMdaClone/Assets/XIVEcsUnityIntegration/Extensions/Ecs/ComponentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using XIV.Ecs;
+
+namespace XIVEcsUnityIntegration.Extensions
+{
+    public static class ComponentExtensions
+    {
+        public static Entity XIVGetEntity(this Component component)
+        {
+            if (component == false) return Entity.Invalid;
+            if (component.TryGetComponent(out GameObjectEntity goEntity)) return goEntity.entity;
+            return Entity.Invalid;
+        }
+    }
+}
---
+++ OpachaMdaClone/Assets/XIVEcsUnityIntegration/Extensions/Ecs/EntityExtensions.cs
@@ -0,0 +1,23 @@
+﻿using XIV.Core.TweenSystem;
+using XIV.Ecs;
+
+namespace XIVEcsUnityIntegration.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static XIVTweenBuilder XIVTween(this Entity entity)
+        {
+            return entity.GetComponent<TransformComp>().transform.XIVTween();
+        }
+
+        public static bool HasTween(this Entity entity)
+        {
+            return entity.GetComponent<TransformComp>().transform.HasTween();
+        }
+
+        public static void CancelTween(this Entity entity)
+        {
+            entity.GetComponent<TransformComp>().transform.CancelTween();
+        }
+    }
+}
