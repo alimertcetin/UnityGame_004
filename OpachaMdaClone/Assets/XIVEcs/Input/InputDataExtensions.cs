8f17fbf0d36050a76c6d36dc76fbf9c79d46d330
alimertcetin <mrtmert54@gmail.com>
2025-10-29 05:52:06 +0300

    feat(InputData): Add InputDataExtensions

---
+++ OpachaMdaClone/Assets/XIVEcs/Input/InputDataExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace XIV.Ecs
+{
+    public static class InputDataExtensions
+    {
+        public static Ray GetInputStartRay(this in InputData inputData, Camera cam)
+        {
+            return cam.ScreenPointToRay(inputData.inputScreenPosStart);
+        }
+        
+        public static Ray GetInputRay(this in InputData inputData, Camera cam)
+        {
+            return cam.ScreenPointToRay(inputData.inputScreenPos);
+        }
+        
+        public static Vector3 InputStartRayAtPlane(this in InputData inputData, Plane plane)
+        {
+            var ray = GetInputStartRay(in inputData, Camera.main);
+            plane.Raycast(ray, out var enter);
+            return ray.GetPoint(enter);
+        }
+        
+        public static Vector3 InputRayAtPlane(this in InputData inputData, Plane plane)
+        {
+            var ray = GetInputRay(inputData, Camera.main);
+            plane.Raycast(ray, out var enter);
+            return ray.GetPoint(enter);
+        }
+    }
+}
